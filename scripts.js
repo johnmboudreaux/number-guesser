ed669f9c7a9edb12773e4f0c28c4b171802efa26
johnmboudreaux <jhnbdrx@gmail.com>
2017-07-06 13:44:35 -0600

    phase 3 complete

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// set game global variables
+// set game global variable
 var userGuess = document.querySelector(".guess-input");
 var guessButton = document.querySelector(".guess-button");
 var clearButton = document.querySelector(".clear-button");
@@ -6,23 +6,41 @@ var resetButton = document.querySelector(".reset-button");
 var yourLastGuessWas = document.querySelector(".your-last-guess-was");
 var lastGuess = document.querySelector(".last-guess");
 var functionOutput = document.querySelector(".function-output");
+var minRange = document.querySelector(".input-min");
+var maxRange = document.querySelector(".input-max");
 var ranNum;
 
 // generate a random number when the page is loaded or re-loaded
+// window.onload = function() {
+//   genRanNum();
+// };
+
 window.onload = function() {
   genRanNum();
+  var input = document.getElementById("button1").focus();
 };
 
 // generates a random number between 0-100 for gameplay
 function genRanNum(){
-  ranNum = Math.floor((Math.random() * 100) + 1);
+    ranNum = Math.floor(Math.random() * 100) + 1;
+    console.log('random num', ranNum);
 }
 
 // event listeners with passed functions following
 userGuess.addEventListener("keyup", checkState);
+
 resetButton.addEventListener("click", reset);
+
 clearButton.addEventListener("click", eraseText);
-guessButton.addEventListener("click", showGuess);
+
+guessButton.addEventListener("click", function() {
+  var min = parseInt(minRange.value);
+      var max = parseInt(maxRange.value);
+        var guess = parseInt(userGuess.value);
+      showGuess();
+    // genRanNum(min, max);
+  compare(userGuess);
+});
 
 // keyup event - check the state of the text input field
 function checkState(event) {
@@ -32,6 +50,7 @@ function checkState(event) {
     enableButtons();
     if(event.keyCode === 13) {
       showGuess();
+      compare();
     }
   }
 }
@@ -49,44 +68,47 @@ function eraseText() {
 
 // click event - display interactive text when a guess is made
 function showGuess() {
-  document.getElementById("your-last-guess-was").innerHTML = "Your last guess was";
-  var userGuessValue = userGuess.value;
-  userGuessValue = parseInt(userGuessValue);
-  lastGuess.innerText = userGuessValue;
-  dataType(userGuessValue);
-  compare(userGuessValue);
+    document.getElementById("your-last-guess-was").innerHTML = "Your last guess was";
+      var userGuessValue = userGuess.value;
+        userGuessValue = parseInt(userGuessValue);
+      lastGuess.innerText = userGuessValue;
+    dataType(userGuessValue);
   minMax(userGuessValue);
 }
 
 // check the data type of the users last guess with another dash of sarcasm
 function dataType(userGuessValue) {
   if(typeof userGuessValue !== "number" || isNaN(userGuessValue))  {
-    lastGuess.innerText = "Really?";
+    lastGuess.innerText = "...";
     alert("you are outside of the required data type. please input a number as the title of this game is 'number guesser'");
   }
 }
 
 // compare the users guess/datatype to random number with a dash of sarcasm
-function compare(userGuessValue) {
-  if(userGuessValue === ranNum) {
-    functionOutput.innerText = "Boom!";
-  }else if(userGuessValue < ranNum) {
+function compare() {
+  var userGuessValue = userGuess.value;
+  userGuessValue = parseInt(userGuessValue);
+  if(userGuessValue < ranNum) {
     functionOutput.innerText = "That is too low";
-  }else if(userGuessValue > ranNum){
-    functionOutput.innerText = "That is too high";
-  }else if(typeof userGuessValue !== "number" || isNaN(userGuessValue)){
-    document.getElementById("your-last-guess-was").innerHTML = "Umm...";
-    functionOutput.innerText = "You know that was not a number";
-  }else{
-    functionOutput.innerText = "";
+  }else if(userGuessValue   > ranNum){
+      functionOutput.innerText = "That is too high";
+  }else if(userGuessValue  === ranNum) {
+      document.getElementById("your-last-guess-was").innerText = " Your range will now increase and decrease by 10"
+      lastGuess.innerText = "BOOM!";
+      functionOutput.innerText = "You guessed it!";
+      genRanNum();
+      minRange.value = parseInt(minRange.value)- 10;
+      maxRange.value = parseInt(maxRange.value)+ 10;
+  }if(typeof userGuessValue !== "number" || isNaN(userGuessValue)){
+    document.getElementById("your-last-guess-was").innerText = "Umm...";
+    functionOutput.innerText = "You know, that was not a number.";
   }
 }
-
 // check to see if the guess is out of min/max range
 function minMax(userGuessValue) {
-  if(userGuessValue < 0) {
+  if(userGuessValue < minRange.value) {
     alert("that is waaaay too low. please guess again");
-}else if(userGuessValue > 100) {
+}else if(userGuessValue > maxRange.value) {
     alert("that is waaaay too high. please guess again");
   }
 }
@@ -100,14 +122,7 @@ function enableButtons() {
 
 // apply disabled state to buttons with exception of reset
 function disableButtons() {
-    document.getElementById("button1").setAttribute("disabled", true);
-    document.getElementById("button2").setAttribute("disabled", true);
-}
-
-// gives the option to reset the game at anytime once it has begun
-function enableDisableResetButton() {
-  if(lastGuess.value === "undefined" || lastGuess.value === "string" || lastGuess.value === "number"){
-    document.getElementById("button3").removeAttribute("disabled");
-  }else{document.getElementById("button3").setAttribute("disabled", true);
-  }
+  document.getElementById("button1").setAttribute("disabled", true);
+  document.getElementById("button2").setAttribute("disabled", true);
+  document.getElementById("button3").setAttribute("disabled", true);
 }
